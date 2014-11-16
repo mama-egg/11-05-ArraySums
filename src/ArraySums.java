553ff9ff96521045a8e281a827fd6c0cb2549de2
Don Irwin <don@falconwebtech.com>
2014-11-15 17:44:06 -0700

    Update ArraySums.java

--- src/ArraySums.java
+++ src/ArraySums.java
@@ -46,14 +46,23 @@ public class ArraySums {
     // COMPLETE to return sum of column in a square 2D array
     public static int ColSum(int col, int[][] grid) {
         int result = 0;
-        
+        for (int row = 0; row < grid[0].length; row++) {
+            result += grid[row][col];
+        }        
         return result;
     }
     
     // COMPLETE to return sum of diagonals in a square 2D array as an array of 2 ints
     public static int[] DiagSums(int[][] grid) {
         int[] result = new int[2];
-        
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0; i < grid.length; i++) {
+            sum1 += grid[i][i];
+            sum2 += grid[i][grid.length - 1 - i];
+        }
+        result[0] = sum1;
+        result[1] = sum2;
         return result;
     }
 }
